5acaf6794e0d842f7267f8b56950982f0657fbec
crx <369383829@qq.com>
2019-12-03 00:11:30 +0800

    s

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+// 文章列表页
+Route::get('/posts', '\App\Http\Controllers\PostController@index');
+// 文章详情页
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
+// 创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
+Route::post('/posts', '\App\Http\Controllers\PostController@store');
+// 编辑文章
+Route::get('/post/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::put('/post/{post}', '\App\Http\Controllers\PostController@update');
+// 删除文章
+Route::get('/post/delete', '\App\Http\Controllers\PostController@delete');
